386ddf29d44b8b96f783b4744fa2c2efaf1a3187
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-05-01 08:46:35 +0530

    multi occur lsearch

---
+++ Java/Recursion/Lsearch with multi occur/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void Lsearch(int nums[], int index, int target, int end, ArrayList<Integer> res) {
+        if (index > end) {
+            return;
+        } else {
+            if (nums[index] == target) {
+                res.add(index);
+                Lsearch(nums, index + 1, target, end, res);
+            }
+            else{
+                Lsearch(nums, index + 1, target, end, res);
+            }
+        
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = new int[] { 1, 2, 3, 4, 5, 5, 6, 7, 5 };
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        Lsearch(nums, 0, 5, n - 1, res);
+        System.out.println(res);
+    }
+}
