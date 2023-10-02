dd25e193006182ca687f44cf1ff4f75f66cfcede
Achyuth Mohan <achyuthmohan1@gmail.com>
2023-10-02 11:27:39 +0530

    dfs

--- Java/Adjacency Matrix Approach/basics/basic/dfs/DFS.java
+++ Java/Adjacency Matrix Approach/basics/basic/dfs/DFS.java
@@ -1,68 +1,59 @@
 import java.util.*;
-class Graph {
-    int n;
-    int adjacency[][];
 
-    Graph(int n) {
-        this.n = n;
-        this.adjacency = new int[n][n];
-    }
-
-    void addEdge(int src, int dest, int wt) {
-        adjacency[src][dest] = wt;
-        adjacency[dest][src] = wt;
-    }
-
-    void removeEdge(int src, int dest) {
-        adjacency[src][dest] = 0;
-        adjacency[dest][src] = 0;
-    }
-
-    void display() {
-        for (int i = 0; i < this.n; i++) {
-            for (int j = 0; j < this.n; j++) {
-                System.out.print(adjacency[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    void dfs(int start){
-        boolean visited[]=new boolean[n];
-        Stack<Integer>stack=new Stack<>();
-        visited[start]=true;
+public class DFS {
+    static void dfs(ArrayList<Integer> res, int adj[][], int start) {
+        Stack<Integer> stack = new Stack<>();
+        boolean visited[] = new boolean[adj.length];
         stack.push(start);
-        System.out.println(start+" ");
-        while(!stack.isEmpty()){
-            int vertex=stack.peek();
-            int nextNeighbor=getNextNeighbour(vertex, visited);
-            if(nextNeighbor==-1){
+        visited[start] = true;
+        res.add(start);
+        while (!stack.isEmpty()) {
+            int temp = stack.peek();
+            int nextNeighbour = getNextNeighbour(adj, temp, visited);
+            if (nextNeighbour == -1) {
                 stack.pop();
-            }
-            else{
-                System.out.print(nextNeighbor+" ");
-                visited[nextNeighbor]=true;
-                stack.push(nextNeighbor);
+            } else {
+                res.add(nextNeighbour);
+                stack.push(nextNeighbour);
+                visited[nextNeighbour] = true;
             }
         }
     }
-    int getNextNeighbour(int vertex,boolean visited[]){
-        for(int i=0;i<n;i++){
-            if(adjacency[vertex][i]==1 && !visited[i]){
+
+    static int getNextNeighbour(int adj[][], int vertex, boolean visited[]) {
+        for (int i = 1; i < visited.length; i++) {
+            if (!visited[i] && adj[vertex][i] == 1) {
                 return i;
             }
         }
         return -1;
     }
-}
 
-public class DFS {
     public static void main(String[] args) {
-        Graph g = new Graph(5);
-        g.addEdge(0, 1, 1);
-        g.addEdge(0, 2, 1);
-        g.addEdge(2, 3, 1);
-        g.display();
-        System.out.println("DFS: ");
-        g.dfs(1);
+        int adj[][] = new int[9][9];
+        adj[1][2] = 1;
+        adj[2][1] = 1;
+        adj[1][3] = 1;
+        adj[3][1] = 1;
+        adj[2][3] = 1;
+        adj[3][2] = 1;
+        adj[4][2] = 1;
+        adj[2][4] = 1;
+        adj[4][5] = 1;
+        adj[5][4] = 1;
+        adj[5][7] = 1;
+        adj[7][5] = 1;
+        adj[6][7] = 1;
+        adj[7][6] = 1;
+        adj[6][8] = 1;
+        adj[8][6] = 1;
+        adj[1][6] = 1;
+        adj[6][1] = 1;
+
+        ArrayList<Integer> res = new ArrayList<>();
+        dfs(res, adj, 1);
+        for(int i=0;i<res.size();i++){
+            System.out.print(res.get(i)+" ");
+        }
     }
 }
